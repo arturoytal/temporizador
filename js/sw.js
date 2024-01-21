b7a1dd45dbb23aad67696cbc2731b3a9ba5f3550
Arturoytal <arturoytal@gmail.com>
2024-01-21 23:40:53 +0100

    algunos ajustes más, que no hacía lo que debía.

--- js/sw.js
+++ js/sw.js
@@ -1,11 +1,34 @@
+const CACHE_NAME = 'version-1';
+const urlsToCache = [ 'index.html', 'css/styles.css', 'js/script.js', 'audio/alarma.mp3' ];
+
 self.addEventListener('install', (event) => {
-    console.log('Service Worker: Instalado');
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then((cache) => {
+                return cache.addAll(urlsToCache);
+            })
+    );
 });
 
 self.addEventListener('fetch', (event) => {
-    console.log('Service Worker: Recuperando');
     event.respondWith(
-        fetch(event.request)
-            .catch(() => caches.match(event.request))
+        caches.match(event.request)
+            .then(() => {
+                return fetch(event.request)
+                    .catch(() => caches.match('/offline.html'));
+            })
+    );
+});
+
+self.addEventListener('activate', (event) => {
+    const cacheWhitelist = [CACHE_NAME];
+    event.waitUntil(
+        caches.keys().then((cacheNames) => Promise.all(
+            cacheNames.map((cacheName) => {
+                if (!cacheWhitelist.includes(cacheName)) {
+                    return caches.delete(cacheName);
+                }
+            })
+        ))
     );
 });
